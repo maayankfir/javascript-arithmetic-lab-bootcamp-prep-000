e62116b459000672086c6dc8e5f337fec2e8dd77
maayankfir <mayankfir@gmail.com>
2018-06-12 10:28:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function add (a, b) {
-return a + b 
+return a + b
 }
 
 function subteact (a,b) {
-return a - b 
+return a - b
 }
 
 function multiply (a,b) {
-return a * b 
+return a * b
 }
 
 function divide (a,b) {
-return a / b 
+return a / b
 }
